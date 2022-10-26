73dfa4a17eb0b3517e050f9938e80d98b3bc25b9
chamalwr <chamalkanishka4@gmail.com>
2022-10-27 02:24:47 +0530

    configuration setting and dependency update

--- library-api-ex-01/library-api-nestjs/src/app.module.ts
+++ library-api-ex-01/library-api-nestjs/src/app.module.ts
@@ -1,7 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp: [
+        {
+          name: 'LibraryAPI',
+          level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+          transport:
+            process.env.NODE_ENV !== 'production'
+              ? { target: 'pino-pretty' }
+              : undefined,
+          autoLogging: false,
+        },
+        process.stdout,
+      ],
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_ATLAS_URL,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- library-api-ex-01/library-api-nestjs/src/main.ts
+++ library-api-ex-01/library-api-nestjs/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger, PinoLogger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
+
+  //Setting pino logger as application level default logger
+  app.useLogger(app.get(Logger));
+
+  const logger = new PinoLogger({});
+  logger.setContext('NestBootsrap');
+
+  const port = process.env.PORT || 3000;
+
+  //Initializing server
+  await app.listen(port, () =>
+    logger.info(`Application started on port ${port}`),
+  );
 }
 bootstrap();
