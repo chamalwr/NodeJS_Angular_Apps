0585de6bacf05a4626651b9205a301a95134c5e2
chamalwr <chamalkanishka4@gmail.com>
2022-10-28 01:38:23 +0530

    implemented author api module - nestjs

--- library-api-ex-01/library-api-nestjs/src/authors/authors.service.ts
+++ library-api-ex-01/library-api-nestjs/src/authors/authors.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
 import { Author, AuthorDocument } from './entities/author.entity';
@@ -10,20 +11,55 @@ export class AuthorsService {
   constructor(
     @InjectModel(Author.name)
     private readonly authorModel: Model<AuthorDocument>,
+    @InjectPinoLogger(AuthorsService.name) private readonly logger: PinoLogger,
   ) {}
-  create(createAuthorDto: CreateAuthorDto) {
-    return 'This action adds a new author';
+  async create(createAuthorDto: CreateAuthorDto) {
+    this.logger.debug(
+      `Creating author with payload ${JSON.stringify(createAuthorDto)}`,
+    );
+    return await this.authorModel.create(createAuthorDto);
   }
 
-  findAll() {
-    return `This action returns all authors`;
+  async findAll(page?: number, take?: number) {
+    const totalAuthors = await this.authorModel.count();
+    if (page && take) {
+      this.logger.debug(
+        `Getting authors paginated sliced into ${page} pages and, ${take} authors for each page`,
+      );
+      const authors = await this.authorModel
+        .find()
+        .limit(take * 1)
+        .skip((page - 1) * take)
+        .exec();
+
+      if (authors) {
+        return {
+          authors,
+          totalPages: Math.ceil(totalAuthors / take),
+          currentPage: page,
+        };
+      }
+      return undefined;
+    }
+    this.logger.debug(
+      `Returning authors unpaginated total ${totalAuthors} records`,
+    );
+    const authors = await this.authorModel.find().exec();
+    return { authors, totalAuthors: totalAuthors };
   }
 
   async findOne(id: string) {
     return await this.authorModel.findById(id);
   }
 
-  update(id: string, updateAuthorDto: UpdateAuthorDto) {
-    return `This action updates a #${id} author`;
+  async update(id: string, updateAuthorDto: UpdateAuthorDto) {
+    this.logger.debug(
+      `Updating author id ${id} with payload ${JSON.stringify(
+        updateAuthorDto,
+      )}`,
+    );
+    return await this.authorModel.findByIdAndUpdate(id, updateAuthorDto, {
+      new: true,
+    });
   }
 }
