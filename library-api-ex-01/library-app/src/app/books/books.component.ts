0429cbdb315ccca91bb556bbaefa209590edac4b
chamalwr <chamalkanishka4@gmail.com>
2022-10-27 20:07:25 +0530

    implemented book cruds on frontend

--- library-api-ex-01/library-app/src/app/books/books.component.ts
+++ library-api-ex-01/library-app/src/app/books/books.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
+import { debounceTime, distinctUntilChanged, filter, firstValueFrom, map, Observable, OperatorFunction } from 'rxjs';
+import { CreateBook } from '../core/dto/create-book.dto';
+import { UpdateBook } from '../core/dto/update-book.dto';
+import { AuthorsService } from '../core/services/authors.service';
 import { BooksService } from '../core/services/books.service';
 
 interface Author {
@@ -16,6 +21,8 @@ interface Book {
   author: Author;
 }
 
+type AuthorType = {  _id: string, firstName: string, lastName: string };
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
@@ -26,9 +33,25 @@ export class BooksComponent implements OnInit {
 	pageSize = 10;
 	collectionSize = 0;
 	books: Book[] = [];
+  authors: Author[] = [];
   loading: boolean = false;
+  currentBookModal: any;
 
-  selectedBook: Book | undefined = {
+  editBookForm = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    isbn: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+    author: new FormControl(null, [Validators.required]),
+  });
+
+  createBookForm = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    isbn: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+    author: new FormControl(null, [Validators.required]),
+  });
+
+  formatter = (state: AuthorType) => state.firstName + " " + state.lastName;
+
+  selectedBook: Book = {
     _id: '',
     name: '',
     isbn: '',
@@ -36,18 +59,20 @@ export class BooksComponent implements OnInit {
       _id: '',
       firstName: '',
       lastName: '',
-    },
-  } || undefined
+    }
+  };
 
   ngOnInit(): void {
     this.getAllBooks(this.page, this.pageSize);
+    this.getAuthors();
   }
 
 	constructor(
     private readonly toastr: ToastrService,
     private modalService: NgbModal,
     public modal: NgbActiveModal,
-    private readonly bookService: BooksService
+    private readonly bookService: BooksService,
+    private readonly authorService: AuthorsService,
   ) {
 		this.refreshBooks();
 	}
@@ -63,12 +88,61 @@ export class BooksComponent implements OnInit {
     this.selectedBook = bookDetail;
     this.modalService.open(content, { centered: true, ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
     }, (close) => {
-      this.selectedBook = undefined;
+      this.selectedBook =  {
+        _id: '',
+        name: '',
+        isbn: '',
+        author: {
+          _id: '',
+          firstName: '',
+          lastName: '',
+        }
+      };
     }).catch((error) => {
-      this.selectedBook = undefined
-    }).finally(() => {
-      this.selectedBook = undefined;
+      this.selectedBook =  {
+        _id: '',
+        name: '',
+        isbn: '',
+        author: {
+          _id: '',
+          firstName: '',
+          lastName: '',
+        }
+      };
     });
+  }
+
+  openCreateBookModal(content: any){
+    this.modalService.open(content, { centered: true, ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      if(this.createBookForm.valid){
+        const createBookDto: CreateBook = {
+          name: this.createBookForm.value.name,
+          isbn: this.createBookForm.value.isbn,
+          author: this.createBookForm.value.author._id,
+        }
+        this.bookService.createABook(createBookDto).subscribe({
+          next: (result: any) => {
+            if(result.loading) {
+              this.loading = true;
+            }
+            this.loading = false;
+            this.toastr.success(`${result.name} is Added`, 'New Book Added!');
+            window.location.reload();
+          },
+          error: (error: any) => {
+            this.loading = false;
+            console.log(error);
+            this.toastr.error(`Creating New Book Failed! : ${error.message}`, 'Internal Server Error');
+          }
+        });
+      }else {
+        this.toastr.warning(`Please check the correct book details and try again`, `User Input Mismatch`)
+      }
+    }, (close) => {
+    }).catch((error) => {
+      console.log(error);
+      this.toastr.error(`Creating New Book Failed!: ${error.message}`, 'Internal Server Error');
+    })
   }
 
   getAllBooks(page: number, take: number){
@@ -88,20 +162,68 @@ export class BooksComponent implements OnInit {
     }));
   }
 
-  getBookById(id: string){
-    this.bookService.getBookById(id).subscribe({
-      next: (result: any) => {
-        if(result.loading){
+  async getBookById(id: string){
+    return await firstValueFrom(this.bookService.getBookById(id)) as Book;
+  }
+
+  getAuthors(){
+    this.authorService.getAuthors(1, 100).subscribe({
+      next: (data: any) => {
+        if(data.loading){
           this.loading = true;
         }
-        this.loading = false;
-        
+
+        this.loading = true;
+        this.authors = data.authors;
       },
       error: (error: any) => {
         this.loading = false;
-        this.toastr.error(`Something went wrong`, 'Internal server error');
+        this.toastr.error(`Something went wrong. Failed to get Authors data`, 'Internal server error');
       }
     });
   }
 
+  async openUpdateBookDataModal(content: any, bookId: string){
+    const bookDetails: Book = await this.getBookById(bookId);
+    this.editBookForm.setValue({
+      name: bookDetails.name,
+      isbn: bookDetails.isbn,
+      author: bookDetails.author,
+    });
+    this.modalService.open(content, { centered: true, ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      if(this.editBookForm.valid){
+        const updateBookDto: UpdateBook = {
+          name: this.editBookForm.value.name,
+          isbn: this.editBookForm.value.isbn,
+          author: this.editBookForm.value.author._id,
+        }
+        this.bookService.updateBook(bookId, updateBookDto).subscribe({
+          next: (result: any) => {
+            if(result.loading) {
+              this.loading = true;
+            }
+            this.loading = false;
+            this.toastr.success(`Book updating completed for ${result.name}`, 'Update Successfull');
+            window.location.reload();
+          },
+          error: (error: any) => {
+            this.loading = false;
+            this.toastr.warning(`Book detail update failed!`, 'Internal Server Error');
+          }
+        });
+      }else {
+        this.toastr.warning(`Please check the correct book details and try again`, `User Input Mismatch`)
+      }
+    }, (close) => {
+    }).catch((error) => {
+      this.toastr.warning(`Book detail update failed!`, 'Internal Server Error');
+    })
+  }
+
+  search: OperatorFunction<string, readonly {_id: string, firstName: string}[]> = (text$: Observable<string>) => text$.pipe(
+    debounceTime(200),
+    distinctUntilChanged(),
+    filter(term => term.length >= 2),
+    map(term => this.authors.filter(category => new RegExp(term, 'mi').test(category.firstName)).slice(0, 10))
+  )
 }
